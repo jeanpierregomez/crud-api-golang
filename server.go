ded9e69c4d804ad716ad7c8278265ffa349101da
CristianCamargo04 <cristianfernandocc@ufps.edu.co>
2021-10-26 23:08:28 -0500

    create auth and jwt (services, controllers)

--- server.go
+++ server.go
@@ -3,19 +3,28 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/ydhnwb/golang_api/config"
+	"github.com/ydhnwb/golang_api/controller"
 	"github.com/ydhnwb/golang_api/repository"
+	"github.com/ydhnwb/golang_api/service"
 	"gorm.io/gorm"
 )
 
 var (
 	db             *gorm.DB                  = config.SetupDatabaseConnection()
+	userRepository repository.UserRepository = repository.NewUserRepository(db)
+	bookRepository repository.BookRepository = repository.NewBookRepository(db)
+	jwtService     service.JWTService        = service.NewJWTService()
+	authService    service.AuthService       = service.NewAuthService(userRepository)
+	authController controller.AuthController = controller.NewAuthController(authService, jwtService)
 )
 
 func main() {
 	defer config.CloseDatabaseConnection(db)
-	userRepository repository.UserRepository = repository.NewUserRepository(db)
-	bookRepository repository.BookRepository = repository.NewBookRepository(db)
 	r := gin.Default()
-
+	authRoutes := r.Group("api/auth")
+	{
+		authRoutes.POST("/login", authController.Login)
+		authRoutes.POST("/register", authController.Register)
+	}
 	r.Run()
 }
