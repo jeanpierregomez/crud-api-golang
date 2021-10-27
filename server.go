5c18abe06f62773aa1119d621fcd82c2add04d50
Jean Pierre <frankjeanpierregs@ufps.edu.co>
2021-10-26 22:53:38 -0500

    add dtos and entities

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/ydhnwb/golang_api/config"
+	"github.com/ydhnwb/golang_api/repository"
 	"gorm.io/gorm"
 )
 
@@ -12,6 +13,8 @@ var (
 
 func main() {
 	defer config.CloseDatabaseConnection(db)
+	userRepository repository.UserRepository = repository.NewUserRepository(db)
+	bookRepository repository.BookRepository = repository.NewBookRepository(db)
 	r := gin.Default()
 
 	r.Run()
